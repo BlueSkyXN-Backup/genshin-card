13283d93e701f66e6347ec142766667117175b2b
BlueSkyXN <xienan0714@gmail.com>
2021-07-15 09:14:30 +0800

    1.1

--- genshincard.sh
+++ genshincard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export COOKIE="account_id=76251146; cookie_token=vME1l1KHa2f48p65BiIGdCJGe4ucD12zikwKUS9A; ltoken=sVSiXmFStGPjfppJRS4o9rmwNU4yIs18LVajoJk8; ltuid=76251146" && yarn serve
+export COOKIE="account_id=76XXXXXX; cookie_token=vME1l1KHa2f4XXXXXXXXXXXXXXXXXXXkwKUS9A; ltoken=sVSiXmFStGPjfppJRS4oXXXXXXXXXXXXXVajoJk8; ltuid=76XXXXXXXX" && yarn serve
